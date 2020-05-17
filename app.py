81a1b32faa2c977dda0f736cc3fbbbadfea58d23
Vikram Shinde <vik.shinde@gmail.com>
2020-05-17 09:11:56 +0100

    Added default port

--- app.py
+++ app.py
@@ -38,4 +38,4 @@ def get_record(id):
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
